c18a95684e1faa4929535ba18331430c8f800efb
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-03-24 16:22:24 -0300

    iniciado configurtação do App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import React from 'react';
 const App = () => {
   return(
     <C.Container>
-      ...
+        <C.Header>
+           <C.HeaderText>Sistema Financeiro</C.HeaderText>
+        </C.Header>
+        <C.Body>
+          ...
+        </C.Body>
     </C.Container>
   )
 }

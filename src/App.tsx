5c12f26d6c4c24044b898d4028feeef245010e13
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-03-25 16:48:04 -0300

    finalizando filtro de lista

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Item} from './types/Item';
 import {Category} from './types/Category';
 import {categories} from './data/categories';
 import {items} from './data/Items';
-import {getCurrentMonth} from './helpers/dateFilter'
+import {getCurrentMonth, filterListByMonth } from './helpers/dateFilter'
 
 const App = () => {
     const [list, setList] = useState(items);
@@ -12,7 +12,7 @@ const App = () => {
     const [currentMonth, setCurrentMonth] = useState(getCurrentMonth);
 
     useEffect(() => {
-      
+      setFilteredList(filterListByMonth(list, currentMonth));
     }, [list, currentMonth]);
 
   return(

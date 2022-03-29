362416549d0b52f16e9cff8a30d869f958e25baf
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-03-29 15:11:05 -0300

    adicionado function de categoria

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,6 +1,9 @@
 import * as C from './styles'
 import { Item } from '../../types/Item'
 import { formatDate } from '../../helpers/dateFilter'
+import { categories } from '../../data/categories';
+
+
 type Props = {
     item: Item
 }
@@ -9,7 +12,7 @@ export const TableItem = ({item}: Props) => {
     return(
         <C.TableLine>
             <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
-            <C.TableColumn>{item.category}</C.TableColumn>
+            <C.TableColumn>{categories[item.category].title}</C.TableColumn>
             <C.TableColumn>{item.title}</C.TableColumn>
             <C.TableColumn> R$ {item.value}</C.TableColumn>
         </C.TableLine>
